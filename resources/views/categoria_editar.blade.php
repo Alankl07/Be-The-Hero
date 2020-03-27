ff657147e5421be32cf60fd5d2a61bccb7541ca0
alan <alan.fsa.ans@gmail.com>
2020-03-27 20:14:30 -0300

    'loja'

---
+++ resources/views/categoria_editar.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.base', ["current"=>"categorias"])
+
+@section('body')
+    <form action = "/categoria/{{$categoria->id}}" method = "POST">
+        @csrf
+        <div class = "form-group">
+            <h1>Edição de categoria</h1>
+            <label for="nome">Nome: </label>
+            <input type = "text" class = "form-control" id="nome" name="nome" value="{{$categoria->nome}}">
+            <br>
+            <button class = "btn btn-primary" type = "submit">Salvar!</button>
+        </div>
+    </form>
+@endsection
